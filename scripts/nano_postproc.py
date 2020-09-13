c64f8664ed5afb2cce034fb240ab124c0cf9164d
ktht <karlehataht@gmail.com>
2020-09-13 20:21:49 +0300

    Remove debugging statements

--- scripts/nano_postproc.py
+++ scripts/nano_postproc.py
@@ -36,28 +36,19 @@ if __name__ == "__main__":
     outdir = args[0]; args = args[1:]
 
     modules = []
-    print("Printing modules")
-    for mod, names in options.imports:
-        print("Module = {}, names = {}".format(mod, names))
+    for mod, names in options.imports: 
         import_module(mod)
         obj = sys.modules[mod]
-        print(obj)
         selnames = names.split(",")
-        print("selnames = {}".format(selnames))
         mods = dir(obj)
-        print(mods)
         for name in selnames:
-            print("name = {}".format(name))
             if name in mods:
-                print("name is in mods")
                 print "Loading %s from %s " % (name, mod)
                 if type(getattr(obj,name)) == list:
                     for mod in getattr(obj,name):
                         modules.append( mod())
                 else:
                     modules.append(getattr(obj,name)())
-            else:
-                print("name is not in mods")
     if options.noOut:
         if len(modules) == 0: 
             raise RuntimeError("Running with --noout and no modules does nothing!")
